c6ccbd2ed944306b8d759d7f20d779b7c51b629a
pilu9147 <119696172+pilu9147@users.noreply.github.com>
2023-07-15 05:50:42 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,38 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [input,setinput] = useState({names:'',mail:'',password:'',confpass:''});
+  const [error,seterror] = useState('')
+  let handlesub = (e)=>{
+    e.preventDefault()
+     if(input.names === '') {
+      return seterror('name cant be empty')
+     }
+     else if(input.mail === '') {
+      return seterror('email cant be empty')
+     }
+     else if(input.password === '' || input.confpass === '') {
+      return seterror('password cant be empty')
+     }
+     else if(input.password !== input.confpass) {
+      return seterror('password mismatched')
+     }
+
+     else seterror('Successfully Created!')
+     setinput({names:'',mail:'',password:'',confpass:''})
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+    <div className="main">
+        <form onSubmit={handlesub}>
+        <input type="text" name="names" placeholder='Full Name' value={input.names} onChange={(e) => setinput({ ...input, names: e.target.value })} /><br />
+        <input type="email" name="mail" placeholder='Email Address' value={input.mail} onChange={(e) => setinput({ ...input, mail: e.target.value })} /><br />
+        <input type="password" name="password" placeholder='Password' value={input.password} onChange={(e) => setinput({ ...input, password: e.target.value })} /><br />
+        <input type="password" name="confpass" placeholder='Confirm Password' value={input.confpass} onChange={(e) => setinput({ ...input, confpass: e.target.value })} /><br />
+        <button type="submit">Create Account</button>
+        {error && <p>{error}</p>}
+        </form>
     </div>
   );
 }
